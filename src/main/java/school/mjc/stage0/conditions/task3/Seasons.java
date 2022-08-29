143eff92e7680f53b77509d26ef2867d0ea17fe0
patifnati <rzezniczekpatrycja@gmail.com>
2022-08-29 22:18:51 +0200

    task done

--- src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
+++ src/main/java/school/mjc/stage0/conditions/task3/Seasons.java
@@ -2,6 +2,24 @@ package school.mjc.stage0.conditions.task3;
 
 public class Seasons {
     public void tellTheSeasonByMonthNumber(int month) {
+        if (month > 2 && month < 6) {
+            System.out.println("Spring");
+        } else {
+            if (month > 5 && month < 9) {
+                System.out.println("Summer");
+            } else {
+                if (month > 8 && month < 12) {
+                    System.out.println("Autumn");
+                } else {
+                    if (month == 12 || month == 1 || month == 2) {
+                        System.out.println("Winter");
+                    } else {
+                        System.out.println("wrong number!");
+                    }
+                }
+            }
 
+        }
     }
 }
+
